8cbd3446ae8fd89d1f12fe3ba6b471f73c9b0406
Jsmaes Yong <86659428+jamesyong042@users.noreply.github.com>
2025-01-16 18:41:43 -0500

    lesson14: camera view transform

--- src/demo.cpp
+++ src/demo.cpp
@@ -22,6 +22,8 @@ namespace vge {
     void Demo::run() {
         SimpleRenderSystem simpleRenderSystem{device, renderer.getSwapChainRenderPass()};
         Camera camera{};
+        // camera.setViewDirection(glm::vec3(0.f), glm::vec3(0.5f, 0.f, 1.f));
+        camera.setViewTarget(glm::vec3(-1.f, -2.f, 2.f), glm::vec3(0.f, 0.f, 2.5f));
 
         while (!window.shouldClose()) {
             glfwPollEvents();
--- src/renderer/SimpleRenderSystem.cpp
+++ src/renderer/SimpleRenderSystem.cpp
@@ -58,13 +58,16 @@ namespace vge {
 
     void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::vector<GameObject> &gameObjects, const Camera &camera) {
         pipeline->bind(commandBuffer);
+
+        auto projectionView = camera.getProjection() * camera.getView();
+
         for (auto& obj : gameObjects) {
             obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.01f, glm::two_pi<float>());
             obj.transform.rotation.x = glm::mod(obj.transform.rotation.x + 0.005f, glm::two_pi<float>());
 
             SimplePushConstantData push{};
             push.color = obj.color;
-            push.transform = camera.getProjection() * obj.transform.mat4();
+            push.transform = projectionView * obj.transform.mat4();
             vkCmdPushConstants(commandBuffer, pipelineLayout,
                                VK_SHADER_STAGE_FRAGMENT_BIT | VK_SHADER_STAGE_VERTEX_BIT, 0,
                                sizeof(SimplePushConstantData), &push);
