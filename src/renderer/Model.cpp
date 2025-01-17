e93e7c2db3573bddaaf3572714c6f3c73b6a4844
Jsmaes Yong <86659428+jamesyong042@users.noreply.github.com>
2025-01-17 00:08:40 -0500

    lesson17: load obj models

--- src/renderer/Model.cpp
+++ src/renderer/Model.cpp
@@ -1,5 +1,26 @@
 #include "Model.hpp"
+#include "utils/Hash.hpp"
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
 #include <cassert>
+#include <cstring>
+#include <iostream>
+#include <unordered_map>
+
+namespace std {
+    template<>
+    struct hash<vge::Model::Vertex> {
+        size_t operator()(vge::Model::Vertex const &vertex) const {
+            size_t seed = 0;
+            vge::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+            return seed;
+        }
+    };
+
+}
 
 namespace vge {
     Model::Model(Device &device, const Builder &builder): device(device) {
@@ -15,6 +36,62 @@ namespace vge {
             vkDestroyBuffer(device.device(), indexBuffer, nullptr);
             vkFreeMemory(device.device(), indexBufferMemory, nullptr);
         }
+    }
+
+    void Model::Builder::loadModel(const std::string &filePath) {
+        tinyobj::attrib_t attrib;
+        std::vector<tinyobj::shape_t> shapes;
+        std::vector<tinyobj::material_t> materials;
+        std::string warn, err;
+        if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filePath.c_str())) {
+            throw std::runtime_error(err + warn);
+        }
+        vertices.clear();
+        indices.clear();
+
+        std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+        for (const auto &shape: shapes) {
+            for (const auto &index: shape.mesh.indices) {
+                Vertex vertex{};
+                if (index.vertex_index >= 0) {
+                    vertex.position = {
+                        attrib.vertices[3 * index.vertex_index + 0],
+                        attrib.vertices[3 * index.vertex_index + 1],
+                        attrib.vertices[3 * index.vertex_index + 2]
+                    };
+                    vertex.color = {
+                        attrib.colors[3 * index.vertex_index + 0],
+                        attrib.colors[3 * index.vertex_index + 1],
+                        attrib.colors[3 * index.vertex_index + 2]
+                    };
+                }
+                if (index.normal_index >= 0) {
+                    vertex.normal = {
+                        attrib.normals[3 * index.normal_index + 0],
+                        attrib.normals[3 * index.normal_index + 1],
+                        attrib.normals[3 * index.normal_index + 2]
+                    };
+                }
+                if (index.texcoord_index >= 0) {
+                    vertex.uv = {
+                        attrib.texcoords[2 * index.texcoord_index + 0],
+                        attrib.texcoords[2 * index.texcoord_index + 1]
+                    };
+                }
+                if (uniqueVertices.count(vertex) == 0) {
+                    uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+                    vertices.push_back(vertex);
+                }
+                indices.push_back(uniqueVertices[vertex]);
+            }
+        }
+    }
+
+    std::unique_ptr<Model> Model::createModelFromFile(Device &device, const std::string &filePath) {
+        Builder builder{};
+        builder.loadModel(filePath);
+        std::cout << "Vertex count: " << builder.vertices.size() << std::endl;
+        return std::make_unique<Model>(device, builder);
     }
 
     void Model::createVertexBuffers(const std::vector<Vertex> &vertices) {
