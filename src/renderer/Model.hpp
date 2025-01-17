ed5d31825ea49232b5a9279494e8a092cd4c7913
Jsmaes Yong <86659428+jamesyong042@users.noreply.github.com>
2025-01-16 22:28:35 -0500

    lesson16:index and staging buffer

--- src/renderer/Model.hpp
+++ src/renderer/Model.hpp
@@ -16,7 +16,12 @@ namespace vge {
             static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
         };
 
-        Model(Device &device, const std::vector<Vertex> &vertices);
+        struct Builder {
+            std::vector<Vertex> vertices;
+            std::vector<uint32_t> indices;
+        };
+
+        Model(Device &device, const Builder &builder);
 
         ~Model();
 
@@ -30,10 +35,17 @@ namespace vge {
 
     private:
         void createVertexBuffers(const std::vector<Vertex> &vertices);
+        void createIndexBuffers(const std::vector<uint32_t> &indices);
 
         Device &device;
         VkBuffer vertexBuffer;
         VkDeviceMemory vertexBufferMemory;
         uint32_t vertexCount;
+
+        VkBuffer indexBuffer;
+        VkDeviceMemory indexBufferMemory;
+        uint32_t indexCount;
+
+        bool hasIndexBuffer = false;
     };
 }
